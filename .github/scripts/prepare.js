5271ed289e1c5069568168895757403665742272
Josh Johanning <joshjohanning@github.com>
2022-09-01 15:27:22 -0500

    team permissions

--- .github/scripts/prepare.js
+++ .github/scripts/prepare.js
@@ -2,11 +2,11 @@ const parseIssueBody = require('./parse-issue-body.js')
 
 module.exports = async ({github, context}) => {
   // const { repositoryName, templateRepository, teamPermission, enableIssues, renameMaster, hasLfs } = parseIssueBody({context})
-  const { repoName, templateRepository, renameMaster, hasLfs } = parseIssueBody({context})
+  const { repoName, templateRepository, teamPermissions, renameMaster, hasLfs } = parseIssueBody({context})
 
   let commentBody
   
-  if (repoName && templateRepository && renameMaster && hasLfs) {
+  if (repoName && templateRepository && teamPermissions && renameMaster && hasLfs) {
   // if (repositoryName && templateRepository && teamPermission && enableIssues && renameMaster && hasLfs) {
     commentBody = `👋 Thank you for opening this migration issue.
   
@@ -14,6 +14,7 @@ module.exports = async ({github, context}) => {
 
     The **repo name** is set to be: **\`${ repoName }\`**
     The **template repository** is set to be: **\`${ templateRepository }\`**
+    The **team permissions** is set to be: **\`${ teamPermissions }\`**
     Are we renaming **master** to **main**? **\`${ renameMaster }\`**
     Is there any **Git LFS** enabled? **\`${ hasLfs }\`**
   
